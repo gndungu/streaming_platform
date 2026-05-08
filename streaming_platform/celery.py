bc2053a10fa122f436d32adc329065fb2e6df48d
root <geoffrey.w.ndungu@gmail.com>
2026-05-09 01:16:40 +0300

    Update requirements files

--- streaming_platform/celery.py
+++ streaming_platform/celery.py
@@ -2,9 +2,9 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'streaming_platform.settings')
 
-app = Celery('project')
+app = Celery('streaming_platform')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
